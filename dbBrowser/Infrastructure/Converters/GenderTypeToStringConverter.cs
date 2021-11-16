86babb8f9a8819f006abcfaf99ac9b9275465a74
Валера Герасимов <valera.gs@bk.ru>
2021-11-17 00:21:04 +0300

    Converters for database enums added.

---
+++ dbBrowser/Infrastructure/Converters/GenderTypeToStringConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+using dbBrowser.Data.Model;
+
+namespace dbBrowser.Infrastructure.Converters
+{
+	public class GenderTypeToStringConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is null)
+				throw new ArgumentNullException(nameof(value));
+
+			if (value is not GenderType gender)
+				throw new NotSupportedException("Type not supported");
+
+			return gender switch {
+				GenderType.Male => "Муж",
+				GenderType.Female => "Жен",
+				_ => throw new ArgumentOutOfRangeException(nameof(value)),
+			};
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value == null)
+				throw new ArgumentNullException(nameof(value));
+
+			if (value is not string strGender)
+				throw new NotSupportedException("Type not supported");
+
+			return strGender switch {
+				"Муж" => GenderType.Male,
+				"Жен" => GenderType.Female,
+				_ => throw new ArgumentOutOfRangeException(nameof(value)),
+			};
+		}
+	}
+}
---
+++ dbBrowser/Infrastructure/Converters/ParentTypeToStringConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using dbBrowser.Data.Model;
+
+namespace dbBrowser.Infrastructure.Converters
+{
+	public class ParentTypeToStringConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is null)
+				throw new ArgumentNullException(nameof(value));
+
+			if (value is not FamilyRelationType parentType)
+				throw new NotSupportedException("Type not supported");
+
+			return parentType switch {
+				FamilyRelationType.Mother => "Мать",
+				FamilyRelationType.Father => "Отец",
+				_ => throw new ArgumentOutOfRangeException(nameof(value)),
+			};
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is null)
+				throw new ArgumentNullException(nameof(value));
+			if (value is not string strValue)
+				throw new NotSupportedException("Type not supported");
+
+			return strValue switch {
+				"Мать" => FamilyRelationType.Mother,
+				"Отец" => FamilyRelationType.Father,
+				_ => throw new ArgumentOutOfRangeException(nameof(value))
+			};
+		}
+	}
+}
