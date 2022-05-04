e54d62d935326bec104eba1de74813f78450bd0f
Валера Герасимов <valera.gs@bk.ru>
2022-05-04 22:52:31 +0300

    Correct "SQL запросы" result view implemented. Dynamic reflection class building for each query result added.

---
+++ dbBrowser/Infrastructure/Utils/DynamicClassGenerator.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace dbBrowser.Infrastructure.Utils
+{
+    public static class DynamicClassGenerator
+    {
+        public static (object instance, Type type) CreateNewObject(IDictionary<string, Type> propertyesParams)
+        {
+            var type = CompileResultType(propertyesParams);
+            var obj = Activator.CreateInstance(type);
+            return (obj, type);
+        }
+
+        public static void FillProperties(object instance, Type instanceType, IEnumerable<object> propsValues)
+        {
+            var propsInfo = instanceType.GetProperties();
+            var currPropVal = propsValues.GetEnumerator();
+
+            if(propsInfo.Length != propsValues.Count())
+                throw new ArgumentException("PropsValues length not equal instanceType propperties count.");
+
+            foreach (var prop in propsInfo)
+            {
+                currPropVal.MoveNext();
+                prop.SetValue(instance, currPropVal.Current);
+            }
+
+        }
+        private static Type CompileResultType(IDictionary<string, Type> propertyesParams)
+        {
+            TypeBuilder tb = GetTypeBuilder();
+
+            foreach (var field in propertyesParams)
+                CreateProperty(tb, field.Key, field.Value);
+
+            Type objectType = tb.CreateType();
+            return objectType;
+        }
+
+        private static TypeBuilder GetTypeBuilder()
+        {
+            var typeSignature = "Type" + Guid.NewGuid();
+            var an = new AssemblyName(typeSignature);
+            AssemblyBuilder assemblyBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(an, AssemblyBuilderAccess.Run);
+            ModuleBuilder moduleBuilder = assemblyBuilder.DefineDynamicModule("MainModule");
+            TypeBuilder tb = moduleBuilder.DefineType(typeSignature,
+                    TypeAttributes.Public |
+                    TypeAttributes.Class |
+                    TypeAttributes.AutoClass |
+                    TypeAttributes.AnsiClass |
+                    TypeAttributes.BeforeFieldInit |
+                    TypeAttributes.AutoLayout,
+                    null);
+            return tb;
+        }
+
+        private static void CreateProperty(TypeBuilder tb, string propertyName, Type propertyType)
+        {
+            FieldBuilder fieldBuilder = tb.DefineField("_" + propertyName, propertyType, FieldAttributes.Private);
+
+            MethodBuilder getPropMthdBldr = tb.DefineMethod("get_" + propertyName,
+                MethodAttributes.Public |
+                MethodAttributes.SpecialName |
+                MethodAttributes.HideBySig,
+                propertyType, Type.EmptyTypes);
+
+            ILGenerator getIL = getPropMthdBldr.GetILGenerator();
+            getIL.Emit(OpCodes.Ldarg_0);
+            getIL.Emit(OpCodes.Ldfld, fieldBuilder);
+            getIL.Emit(OpCodes.Ret);
+
+            MethodBuilder setPropMthdBldr =
+                tb.DefineMethod("set_" + propertyName,
+                  MethodAttributes.Public |
+                  MethodAttributes.SpecialName |
+                  MethodAttributes.HideBySig,
+                  null, new[] { propertyType });
+
+            ILGenerator setIL = setPropMthdBldr.GetILGenerator();
+            Label modifyProperty = setIL.DefineLabel();
+            Label exitSet = setIL.DefineLabel();
+
+            setIL.MarkLabel(modifyProperty);
+            setIL.Emit(OpCodes.Ldarg_0);
+            setIL.Emit(OpCodes.Ldarg_1);
+            setIL.Emit(OpCodes.Stfld, fieldBuilder);
+
+            setIL.Emit(OpCodes.Nop);
+            setIL.MarkLabel(exitSet);
+            setIL.Emit(OpCodes.Ret);
+
+            PropertyBuilder propertyBuilder = tb.DefineProperty(propertyName, PropertyAttributes.HasDefault, propertyType, null);
+            propertyBuilder.SetGetMethod(getPropMthdBldr);
+            propertyBuilder.SetSetMethod(setPropMthdBldr);
+        }
+    }
+}
--- dbBrowser/ViewModels/Data/QueryPageViewModel.cs
+++ dbBrowser/ViewModels/Data/QueryPageViewModel.cs
@@ -3,215 +3,239 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Documents;
 using System.Windows.Input;
 using dbBrowser.Commands;
 using dbBrowser.Data.Model;
+using dbBrowser.Infrastructure.Utils;
 using dbBrowser.ViewModels.Base;
 using dbBrowser.Views.Windows;
 
 namespace dbBrowser.ViewModels.Data
 {
-	public class QueryPageViewModel : BaseViewModel
-	{
-		private readonly UniversityDataBaseContainer _Db;
-		private string _Query;
-		private IEnumerable _ResultItems;
-		private string _QueryStringResult;
+    public class QueryPageViewModel : BaseViewModel
+    {
+        private readonly UniversityDataBaseContainer _Db;
+        private string _Query;
+        private IEnumerable _ResultItems;
+        private string _QueryStringResult;
 
-		public QueryPageViewModel()
-		{
-			_Db = new();
+        public QueryPageViewModel()
+        {
+            _Db = new();
 
-			#region Commands
+            #region Commands
 
-			GetTheOldestStudentsCommand =
-				new LambdaCommand(OnGetTheOldestStudentsCommandExecuted, o => true);
+            GetTheOldestStudentsCommand =
+                new LambdaCommand(OnGetTheOldestStudentsCommandExecuted, o => true);
 
-			GetTwoParentsStudentsCommand =
-				new LambdaCommand(OnGetTwoParentsStudentsCommandExecuted, o => true);
+            GetTwoParentsStudentsCommand =
+                new LambdaCommand(OnGetTwoParentsStudentsCommandExecuted, o => true);
 
-			FindGroupsWithOrphanCommand =
-				new LambdaCommand(OnFindGroupsWithOrphanCommandExecuted, o => true);
+            FindGroupsWithOrphanCommand =
+                new LambdaCommand(OnFindGroupsWithOrphanCommandExecuted, o => true);
 
-			StudentsByPatronymicCommand =
-				new LambdaCommand(OnStudentsByPatronymicCommandExecuted, o => true);
+            StudentsByPatronymicCommand =
+                new LambdaCommand(OnStudentsByPatronymicCommandExecuted, o => true);
 
-			GetFacultyByGroupNameCommand =
-				new LambdaCommand(OnGetFacultyByGroupNameCommandExecuted, o => true);
+            GetFacultyByGroupNameCommand =
+                new LambdaCommand(OnGetFacultyByGroupNameCommandExecuted, o => true);
 
-			GetParentsByStudentIdCommand =
-				new LambdaCommand(OnGetParentsByStudentIdCommandExecuted, o => true);
+            GetParentsByStudentIdCommand =
+                new LambdaCommand(OnGetParentsByStudentIdCommandExecuted, o => true);
 
-			ExecuteSqlQueryCommand =
-				new LambdaCommand(OnExecuteSqlQueryCommandExecuted, o => true);
+            ExecuteSqlQueryCommand =
+                new LambdaCommand(OnExecuteSqlQueryCommandExecuted, o => true);
 
-			#endregion
+            #endregion
 
-		}
+        }
 
-		public string Query {
-			get => _Query;
-			set => Set(ref _Query, value);
-		}
+        public string Query
+        {
+            get => _Query;
+            set => Set(ref _Query, value);
+        }
 
-		public IEnumerable ResultItems {
-			get => _ResultItems;
-			set => Set(ref _ResultItems, value);
-		}
+        public IEnumerable ResultItems
+        {
+            get => _ResultItems;
+            set => Set(ref _ResultItems, value);
+        }
 
-		public string QueryStringResult {
-			get => _QueryStringResult;
-			set => Set(ref _QueryStringResult, value);
-		}
+        public string QueryStringResult
+        {
+            get => _QueryStringResult;
+            set => Set(ref _QueryStringResult, value);
+        }
 
-		#region Commands
+        #region Commands
 
-		public ICommand GetTheOldestStudentsCommand { get; set; }
+        public ICommand GetTheOldestStudentsCommand { get; set; }
 
-		private void OnGetTheOldestStudentsCommandExecuted(object p)
-		{
-			var q =
-				_Db.Students.OrderByDescending(s => s.Birthday).Take(3)
-					.Select(s => new { s.Id, s.Surname, s.Name, s.StudyGroup.Title });
-			Query = q.ToString();
-			ResultItems = q.ToList();
-		}
+        private void OnGetTheOldestStudentsCommandExecuted(object p)
+        {
+            var q =
+                _Db.Students.OrderByDescending(s => s.Birthday).Take(3)
+                    .Select(s => new { s.Id, s.Surname, s.Name, s.StudyGroup.Title });
+            Query = q.ToString();
+            ResultItems = q.ToList();
+        }
 
-		public ICommand GetTwoParentsStudentsCommand { get; set; }
+        public ICommand GetTwoParentsStudentsCommand { get; set; }
 
-		private void OnGetTwoParentsStudentsCommandExecuted(object p)
-		{
-			var q =
-				_Db.FamilyRelations.GroupBy(fr => fr.Student).Where(g => g.Count() == 2)
-					.Select(g => g.Key)
-					.Select(s => new { s.Id, s.Surname, s.Name, s.StudyGroup.Title });
-			Query = q.ToString();
-			ResultItems = q.ToList();
-		}
+        private void OnGetTwoParentsStudentsCommandExecuted(object p)
+        {
+            var q =
+                _Db.FamilyRelations.GroupBy(fr => fr.Student).Where(g => g.Count() == 2)
+                    .Select(g => g.Key)
+                    .Select(s => new { s.Id, s.Surname, s.Name, s.StudyGroup.Title });
+            Query = q.ToString();
+            ResultItems = q.ToList();
+        }
 
-		public ICommand FindGroupsWithOrphanCommand { get; set; }
+        public ICommand FindGroupsWithOrphanCommand { get; set; }
 
-		private void OnFindGroupsWithOrphanCommandExecuted(object p)
-		{
-			var q =
-				_Db.StudyGroups.Include("Students")
-					.Select(g => new { Group = g.Title, Count = g.Students.Count(s => s.Privileges.Any(p => p.Title == "Сирота")) })
-					.Where(at => at.Count > 0);
-			Query = q.ToString();
-			ResultItems = q.ToList();
-		}
+        private void OnFindGroupsWithOrphanCommandExecuted(object p)
+        {
+            var q =
+                _Db.StudyGroups.Include("Students")
+                    .Select(g => new { Group = g.Title, Count = g.Students.Count(s => s.Privileges.Any(p => p.Title == "Сирота")) })
+                    .Where(at => at.Count > 0);
+            Query = q.ToString();
+            ResultItems = q.ToList();
+        }
 
-		//Massage boxes
+        //Massage boxes
 
-		public ICommand StudentsByPatronymicCommand { get; set; }
+        public ICommand StudentsByPatronymicCommand { get; set; }
 
-		private void OnStudentsByPatronymicCommandExecuted(object p)
-		{
-			var qRes = ShowQueryDialog("Введите отчество:");
-			if (string.IsNullOrWhiteSpace(qRes))
-				return;
+        private void OnStudentsByPatronymicCommandExecuted(object p)
+        {
+            var qRes = ShowQueryDialog("Введите отчество:");
+            if (string.IsNullOrWhiteSpace(qRes))
+                return;
 
-			var q =
-				_Db.Students.Where(s => s.Patronymic == qRes);
-			Query = q.ToString();
-			ResultItems = q.ToList();
-		}
+            var q =
+                _Db.Students.Where(s => s.Patronymic == qRes);
+            Query = q.ToString();
+            ResultItems = q.ToList();
+        }
 
-		public ICommand GetParentsByStudentIdCommand { get; set; }
+        public ICommand GetParentsByStudentIdCommand { get; set; }
 
-		private void OnGetParentsByStudentIdCommandExecuted(object p)
-		{
-			var qRes = ShowQueryDialog("Введите Id студента:");
-			if (string.IsNullOrWhiteSpace(qRes))
-				return;
+        private void OnGetParentsByStudentIdCommandExecuted(object p)
+        {
+            var qRes = ShowQueryDialog("Введите Id студента:");
+            if (string.IsNullOrWhiteSpace(qRes))
+                return;
 
-			var qId = int.Parse(qRes);
+            var qId = int.Parse(qRes);
 
-			var q =
-				_Db.FamilyRelations.Include("Student").Include("StudentParent")
-					.Where(fr => fr.Student.Id == qId)
-					.Select(fr => new { fr.StudentParent.Id, fr.StudentParent.Surname, fr.StudentParent.Name });
-			Query = q.ToString();
-			ResultItems = q.ToList();
-		}
+            var q =
+                _Db.FamilyRelations.Include("Student").Include("StudentParent")
+                    .Where(fr => fr.Student.Id == qId)
+                    .Select(fr => new { fr.StudentParent.Id, fr.StudentParent.Surname, fr.StudentParent.Name });
+            Query = q.ToString();
+            ResultItems = q.ToList();
+        }
 
-		public ICommand GetFacultyByGroupNameCommand { get; set; }
+        public ICommand GetFacultyByGroupNameCommand { get; set; }
 
-		private void OnGetFacultyByGroupNameCommandExecuted(object p)
-		{
-			var qRes = ShowQueryDialog("Введите название группы:");
-			if (string.IsNullOrWhiteSpace(qRes))
-				return;
+        private void OnGetFacultyByGroupNameCommandExecuted(object p)
+        {
+            var qRes = ShowQueryDialog("Введите название группы:");
+            if (string.IsNullOrWhiteSpace(qRes))
+                return;
 
-			var q =
-				_Db.StudyGroups.Include("Faculty").Where(sg => sg.Title == qRes)
-					.Select(sg => new { sg.Faculty.Id, sg.Faculty.Title });
-			Query = q.ToString();
-			ResultItems = q.ToList();
-		}
+            var q =
+                _Db.StudyGroups.Include("Faculty").Where(sg => sg.Title == qRes)
+                    .Select(sg => new { sg.Faculty.Id, sg.Faculty.Title });
+            Query = q.ToString();
+            ResultItems = q.ToList();
+        }
 
-		private string ShowQueryDialog(string inputTitle)
-		{
-			MainDialogWindow qWindow = new();
-			qWindow.InputText.Text = inputTitle;
-			qWindow.ShowDialog();
-			return qWindow.ResultText.Text;
-		}
+        private string ShowQueryDialog(string inputTitle)
+        {
+            MainDialogWindow qWindow = new();
+            qWindow.InputText.Text = inputTitle;
+            qWindow.ShowDialog();
+            return qWindow.ResultText.Text;
+        }
 
-		public ICommand ExecuteSqlQueryCommand { get; set; }
+        public ICommand ExecuteSqlQueryCommand { get; set; }
 
-		private void OnExecuteSqlQueryCommandExecuted(object p)
-		{
-			string firstWord = Query.Split(' ')[0].ToLower();
-			if (firstWord is "insert" or "update" or "delete") {
-				int RowsAffected = _Db.Database.ExecuteSqlCommand(Query);
-				ResultItems = new List<object> { new { RowsAffected } };
-				return;
-			}
+        private void OnExecuteSqlQueryCommandExecuted(object p)
+        {
+            Query = Regex.Replace(Query, "\\s{2,}", " ");
 
-			//ResultItems = _Db.Database.SqlQuery<object>(Query).ToList();
-			var objs = new object[10];
-			var resList = new List<object>(10);
-			using (SqlConnection connection = new SqlConnection(_Db.Database.Connection.ConnectionString)) {
-				SqlCommand command = new SqlCommand(Query, connection);
-				try {
-					connection.Open();
-					SqlDataReader reader = command.ExecuteReader();
-					while (reader.Read())
-					{
-						QueryStringResult += reader.GetValues(objs);
-						resList.Add(
-							new
-							{
-								col0 = objs[0],
-								col1 = objs[1],
-								col2 = objs[2],
-								col3 = objs[3],
-								col4 = objs[4],
-								col5 = objs[5],
-								col6 = objs[6],
-								col7 = objs[7],
-								col8 = objs[8],
-								col9 = objs[9]
-							});
-					}
-					reader.Close();
-				}
-				catch (Exception e)
-				{
-					MessageBox.Show(e.Message);
-				}
-				finally {
-					connection.Close();
-				}
-			}
+            string firstWord = Query.Split(' ')[0].ToLower();
+            if (firstWord is "insert" or "update" or "delete")
+            {
+                int RowsAffected = _Db.Database.ExecuteSqlCommand(Query);
+                ResultItems = new List<object> { new { RowsAffected } };
+                return;
+            }
 
-			ResultItems = resList;
-		}
+            if(Query.ToUpper().Contains("GROUP BY"))
+            {
+                MessageBox.Show("GROUP BY currently not supported", "Предупреждение", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
 
-		#endregion
+            var objs = new object[32];
+            var resList = new List<object>(10);
+            using (SqlConnection connection = new SqlConnection(_Db.Database.Connection.ConnectionString))
+            {
+                SqlCommand command = new SqlCommand(Query, connection);
+                try
+                {
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (!reader.Read())
+                            return;
 
-	}
+                        int columnsCount = reader.GetValues(objs);
+                        var values = new object[columnsCount];
+                        for (int i = 0; i < columnsCount; i++)
+                            values[i] = objs[i];
+
+                        Dictionary<string, Type> props = new(10);
+
+                        for (int i = 0; i < columnsCount; ++i)
+                            props.Add(reader.GetName(i), reader.GetFieldType(i));
+
+                        var (toAddObj, type) = DynamicClassGenerator.CreateNewObject(props);
+                        DynamicClassGenerator.FillProperties(toAddObj, type, values);
+                        resList.Add(toAddObj);
+
+                        while (reader.Read())
+                        {
+                            reader.GetValues(values);
+
+                            (toAddObj, type) = DynamicClassGenerator.CreateNewObject(props);
+                            DynamicClassGenerator.FillProperties(toAddObj, type, values);
+                            resList.Add(toAddObj);
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+
+            ResultItems = resList;
+        }
+
+        #endregion
+
+    }
 }
